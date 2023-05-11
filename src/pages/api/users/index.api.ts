d5dee3648f4f180e279d51c9a39cbe4675691b4f
giovani <giovaniocan@gmail.com>
2023-05-11 17:18:41 -0300

    it's possible create an user

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,6 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { prisma } from '@/lib/prisma'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json(req.body)
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
+
+  const { name, username } = req.body
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      username,
+    },
+  })
+
+  return res.status(201).json(user)
 }
