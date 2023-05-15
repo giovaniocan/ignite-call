eb3f6e0c9bae4661598409ae786d14b9ba4710ea
giovani <giovaniocan@gmail.com>
2023-05-15 19:58:15 -0300

    making the prisma adpter

---
+++ src/lib/auth/prisma-adapter.ts
@@ -0,0 +1,162 @@
+import { Adapter } from 'next-auth/adapters'
+import { prisma } from '../prisma'
+
+export function PrismaAdapter(): Adapter {
+  return {
+    async createUser(user) {},
+
+    async getUser(id) {
+      const user = await prisma.user.findUniqueOrThrow({
+        where: {
+          id,
+        },
+      })
+
+      return {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        email: user.email!,
+        avatar_url: user.avatar_url!, // esse ponto de exclamação é pra dizer ao typescript que isso vai ser informado, e não sera nulo
+        emailVerified: null,
+      }
+    },
+    async getUserByEmail(email) {
+      const user = await prisma.user.findUniqueOrThrow({
+        where: {
+          email,
+        },
+      })
+
+      return {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        email: user.email!,
+        avatar_url: user.avatar_url!, // esse ponto de exclamação é pra dizer ao typescript que isso vai ser informado, e não sera nulo
+        emailVerified: null,
+      }
+    },
+    async getUserByAccount({ providerAccountId, provider }) {
+      const { user } = await prisma.account.findUniqueOrThrow({
+        // e sse metodo se não encontrar nada vai disparar um erro, então é certeza que vai ter um dado, ele não tem a possivilidade de ser nulo
+        where: {
+          provider_provider_account_id: {
+            provider,
+            provider_account_id: providerAccountId,
+          },
+        },
+        include: {
+          user: true,
+        },
+      })
+      return {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        email: user.email!,
+        avatar_url: user.avatar_url!, // esse ponto de exclamação é pra dizer ao typescript que isso vai ser informado, e não sera nulo
+        emailVerified: null,
+      }
+    },
+
+    async updateUser(user) {
+      const prismaUser = await prisma.user.update({
+        where: {
+          id: user.id!,
+        },
+        data: {
+          name: user.name,
+          email: user.email,
+          avatar_url: user.avatar_url,
+        },
+      })
+      return {
+        id: prismaUser.id,
+        name: prismaUser.name,
+        username: prismaUser.username,
+        email: prismaUser.email!,
+        avatar_url: prismaUser.avatar_url!, // esse ponto de exclamação é pra dizer ao typescript que isso vai ser informado, e não sera nulo
+        emailVerified: null,
+      }
+    },
+
+    async linkAccount(account) {
+      await prisma.account.create({
+        data: {
+          user_id: account.userId,
+          type: account.type,
+          provider: account.provider,
+          provider_account_id: account.providerAccountId,
+          refresh_token: account.refresh_token,
+          access_token: account.access_token,
+          expires_at: account.expires_at,
+          token_type: account.token_type,
+          scope: account.scope,
+          id_token: account.id_token,
+          session_state: account.session_state,
+        },
+      })
+    },
+
+    async createSession({ sessionToken, userId, expires }) {
+      await prisma.session.create({
+        data: {
+          user_id: userId,
+          expires,
+          session_token: sessionToken,
+        },
+      })
+      return {
+        userId,
+        sessionToken,
+        expires,
+      }
+    },
+
+    async getSessionAndUser(sessionToken) {
+      const { user, ...session } = await prisma.session.findFirstOrThrow({
+        //* ta tirando o user e todo restante ta colocando em session*//
+        where: {
+          session_token: sessionToken,
+        },
+        include: {
+          // aqui eu to trazendo os dados do usuario relacionado a essa sessao
+          user: true,
+        },
+      })
+      return {
+        session: {
+          expires: session.expires,
+          sessionToken: session.session_token,
+          userId: session.user_id,
+        },
+        user: {
+          id: user.id,
+          name: user.name,
+          username: user.username,
+          email: user.email!,
+          avatar_url: user.avatar_url!, // esse ponto de exclamação é pra dizer ao typescript que isso vai ser informado, e não sera nulo
+          emailVerified: null,
+        },
+      }
+    },
+
+    async updateSession({ sessionToken, userId, expires }) {
+      const prismaSession = await prisma.session.update({
+        where: {
+          session_token: sessionToken,
+        },
+        data: {
+          expires,
+          user_id: userId,
+        },
+      })
+      return {
+        sessionToken: prismaSession.session_token,
+        userId: prismaSession.user_id,
+        expires: prismaSession.expires,
+      }
+    },
+  }
+}
