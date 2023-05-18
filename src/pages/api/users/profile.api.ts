c6ff388b2b6bb362641be26589e34b502b24f14c
giovani <giovaniocan@gmail.com>
2023-05-18 15:21:32 -0300

    add  bio in back-end

---
+++ src/pages/api/users/profile.api.ts
@@ -0,0 +1,44 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getServerSession } from 'next-auth'
+import { buildNextAuthOptions } from '../auth/[...nextauth].api'
+import { z } from 'zod'
+import { prisma } from '@/lib/prisma'
+
+const upudateProfileBodySchema = z.object({
+  bio: z.string(),
+})
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'PUT') {
+    return res.status(405).end()
+  }
+
+  const session = await getServerSession(
+    // aqui é para pegar a sessão do usuário (dados)
+    req,
+    res,
+    buildNextAuthOptions(req, res),
+  )
+
+  if (!session) {
+    return res.status(401).end()
+  }
+
+  const { bio } = upudateProfileBodySchema.parse(req.body) // para tipar os dados, e se não der certo ele automaticamente ja dispara um erro, se quiser que não dispare o erro use o safeParse
+
+  /*  await prisma.userTimeInterval.createMany // se fosse em outro banco de dados era so fazer isso para adicionar todos os intervalos */
+
+  await prisma.user.update({
+    where: {
+      id: session.user.id,
+    },
+    data: {
+        bio,
+    },
+  })
+
+  return res.status(204).end()
+}
