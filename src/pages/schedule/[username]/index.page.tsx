73c0ebb7dd709a05e73d432f8af80025f8786ba9
giovani <giovaniocan@gmail.com>
2023-05-18 16:14:14 -0300

    page css is ready, but the calendar is missing

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,58 @@
+import { Avatar, Heading, Text } from '@ignite-ui/react'
+import { Container, UserHeader } from './styles'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { prisma } from '@/lib/prisma'
+
+interface ScheduleProps {
+  user: {
+    name: string
+    bio: string
+    avatarUrl: string
+  }
+}
+
+export default function Schedule({ user }: ScheduleProps) {
+  return (
+    <Container>
+      <UserHeader>
+        <Avatar src={user.avatarUrl} />
+        <Heading>{user.name}</Heading>
+        <Text>{user.bio}</Text>
+      </UserHeader>
+    </Container>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [], // não gera nenhuma pagina estatic na build, e sim conforme o povo vai acessando7
+    fallback: 'blocking', // o usuario so entra na pgina quando estiver tudo gerado de forma estatica
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username) // para pegar a requisão que esta na url, pois quando a pagina é estatica ele não tem req e res
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      user: {
+        name: user.name,
+        bio: user.bio,
+        avatarUrl: user.avatar_url,
+      },
+    },
+    revalidate: 60 * 60 * 24, // 1 day
+  }
+}
