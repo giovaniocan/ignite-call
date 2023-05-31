45ed8d27f255774d7ad318b2428a0688d74f2ea9
giovani <giovaniocan@gmail.com>
2023-05-31 14:13:09 -0300

    fix the UTC date's problem

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -7,6 +7,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
+  console.log(req.method)
   if (req.method !== 'POST') {
     return res.status(405).end()
   }
